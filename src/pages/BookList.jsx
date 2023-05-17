a25532a9adbd1af65374d46f23eca33facfd8bc9
Marija Carapic <mc20190116@student.fon.bg.ac.rs>
2023-05-18 00:23:19 +0200

    Book List- Konacna struktura

--- src/pages/BookList.jsx
+++ src/pages/BookList.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 import '../App';
+import {API_URL} from '../API';
+import axios from 'axios';
 
 const BookList = () => {
-  return (
-    <div>Book List</div>
-  );
+
+const [books, setBooks] = useState([]);
+
+useEffect(() => {
+  axios
+   .get(API_URL)
+   .then(res => {
+      console.log(res.data)
+      setBooks(res.data)
+  })
+    .catch(err=>console.log(err))
+  }, []);
+
+  return <div className="book-list">
+    {books.map((book)=> (
+      <div key = {book.id} className = "book">
+        <div><h4><i>{book.title} </i></h4></div>
+        <div><img src= {book.image_url} alt = "#"/> </div>
+        <div> <button><b>Add to Favourites </b></button> </div>
+
+        </div>
+    ))}
+    </div>;
+ 
 };
 
 export default BookList;
