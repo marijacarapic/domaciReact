b4e519de9f2030807e422d7b81af36b1465bcf4a
Marija Carapic <mc20190116@student.fon.bg.ac.rs>
2023-05-18 14:37:47 +0200

    Favourite stranica i njeno stilizovanje

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,9 +1,45 @@
 import React from "react";
 import '../App';
+import { useAppContext } from "../context/appContext";
 
 const Favorites = () => {
+
+  const {favorites, addToFavourites, removeFromFavourites} = useAppContext();
+
+  console.log("favourites", favorites);
+
+   const favoritesChecker = (id) => {
+   const boolean = favorites.some((book) => book.id ===id);
+
+     return boolean;
+   };
+
   return (
-    <div>Favorites</div>
+ <div className="favorites">
+       {favorites.length > 0 ? favorites.map((book)=> (
+      <div key = {book.id} className = "book">
+        <div>
+          <h4><i>{book.title} </i></h4>
+          </div>
+        <div>
+          <img src= {book.image_url} alt = "#"/> 
+          </div>
+        <div>
+          {favoritesChecker(book.id) ? (
+          <button onClick={()=> removeFromFavourites(book.id)} >
+           Remove from Favorites 
+          </button>
+      ) : (
+          <button onClick={()=> addToFavourites(book)} >
+               Add to Favorites 
+          </button>
+      
+        )}
+          </div>
+
+        </div>
+    )) : <h4> You don't have any favorites book yet!</h4>}
+    </div>
   );
 };
 
