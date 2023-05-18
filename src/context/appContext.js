c7a64997731c13f8b9ff446e9ac9153c58c13420
Marija Carapic <mc20190116@student.fon.bg.ac.rs>
2023-05-18 13:30:53 +0200

    Dodavanje funkcionalnosti - add and remove button

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -6,8 +6,8 @@ const AppContext = createContext(null);
 export const useAppContext = () => {
     const context = useContext(AppContext);
 
-    if(context == undefined) {
-        throw new Error;
+    if(context === undefined) {
+        throw new Error('Appcontext');
 
     }
     return context;
@@ -17,17 +17,26 @@ const AppContextProvider = ({children}) => {
 
     const [favorites, setFavorites] = useState([]);
      
-    const addToFavourites = () => {
+    const addToFavourites = (book) => {
+       const oldFavorites = [...favorites];
 
+       const newFavorites = oldFavorites.concat(book);
+
+       setFavorites(newFavorites);
     };
 
     const removeFromFavourites = (id) => {
-        
+        const oldFavorites = [...favorites];
+        const newFavorites = oldFavorites.filter((book) => book.id !== id);
+
+        setFavorites(newFavorites);
     };
 
      return (
 
-        <AppContext.Provider value = {{favorites, addToFavourites, removeFromFavourites}}>
+        <AppContext.Provider 
+        value = {{favorites, addToFavourites, removeFromFavourites}}
+        >
             {children}
         </AppContext.Provider>
     
--- src/pages/BookList.jsx
+++ src/pages/BookList.jsx
@@ -2,32 +2,60 @@ import React,{useState, useEffect} from "react";
 import '../App';
 import {API_URL} from '../API';
 import axios from 'axios';
+import { useAppContext } from "../context/appContext";
 
 const BookList = () => {
 
-const [books, setBooks] = useState([]);
+  const [books, setBooks] = useState([]);
+
+  const {favorites, addToFavourites, removeFromFavourites} = useAppContext();
+
+console.log("favourites", favorites);
+
+const favoritesChecker = (id) => {
+  const boolean = favorites.some((book) => book.id ===id);
+
+  return boolean;
+};
 
 useEffect(() => {
   axios
    .get(API_URL)
-   .then(res => {
-      console.log(res.data)
-      setBooks(res.data)
+   .then((res) => {
+      console.log(res.data);
+      setBooks(res.data);
   })
-    .catch(err=>console.log(err))
+    .catch((err)=>console.log(err));
   }, []);
 
-  return <div className="book-list">
+  return (
+   <div className="book-list">
     {books.map((book)=> (
       <div key = {book.id} className = "book">
-        <div><h4><i>{book.title} </i></h4></div>
-        <div><img src= {book.image_url} alt = "#"/> </div>
-        <div> <button><b>Add to Favourites </b></button> </div>
+        <div>
+          <h4><i>{book.title} </i></h4>
+          </div>
+        <div>
+          <img src= {book.image_url} alt = "#"/> 
+          </div>
+        <div>
+          {favoritesChecker(book.id) ? (
+          <button onClick={()=> removeFromFavourites(book.id)} >
+           Remove from Favorites 
+          </button>
+      ) : (
+          <button onClick={()=> addToFavourites(book)} >
+               Add to Favorites 
+          </button>
+      
+        )}
+          </div>
 
         </div>
     ))}
-    </div>;
- 
+    </div>
+
+  );
 };
 
 export default BookList;
